651dfe9b88f2b50381ae3cf7692997fda83a7d77
Slava Nossar <slava.nossar@gmail.com>
2022-09-08 13:39:06 +1000

    add template files

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  content: [
+    'components/**/*.vue',
+    'layouts/**/*.vue',
+    'pages/**/*.vue',
+    'app.vue',
+    'error.vue',
+    'nuxtClasses.js',
+  ],
+  theme: {
+    fontFamily: {
+      body: [
+        'Sample',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
