4be4991d959ebdc1f0ea5cf9abec125b49e34e12
Slava Nossar <slava.nossar@gmail.com>
2022-09-08 14:22:26 +1000

    move app head to app.vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,6 @@
 import { defineNuxtConfig } from 'nuxt'
 
-import nuxtClasses from './nuxtClasses'
-
 export default defineNuxtConfig({
-  app: {
-    head: {
-      bodyAttrs: {
-        class: nuxtClasses.body,
-      },
-      htmlAttrs: {
-        class: nuxtClasses.html,
-        lang: 'en',
-      },
-      meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      ],
-    },
-  },
   css: [],
   modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
   buildModules: ['vite-plugin-vue-type-imports/nuxt'],
