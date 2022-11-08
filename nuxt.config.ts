7276fe25734e41725f09de1150af95de9fcfa3b2
Slava Nossar <slava.nossar@gmail.com>
2022-11-08 18:13:06 +1100

    define layout + page transitions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 export default defineNuxtConfig({
+  app: {
+    layoutTransition: { name: 'layout', mode: 'out-in' },
+    pageTransition: { name: 'page', mode: 'out-in' },
+  },
   css: [],
   modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
   buildModules: ['vite-plugin-vue-type-imports/nuxt'],
