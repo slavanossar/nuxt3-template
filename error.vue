5fba7e3b7fb684e2f35f6b394754d758caf60adb
Slava Nossar <slava.nossar@gmail.com>
2023-07-21 15:59:46 +1000

    better error.vue template

--- error.vue
+++ error.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
+    <p v-text="props.error.message" />
     <button @click="handleError">Clear errors</button>
   </div>
 </template>
 
 <script lang="ts" setup>
-interface Props {
-  error: object
-}
-const props = defineProps<Props>()
+import type { H3Error } from 'h3'
 
+interface Props {
+  error: H3Error
+}
+
+const props = defineProps<Props>()
 const handleError = () => clearError({ redirect: '/' })
 </script>
